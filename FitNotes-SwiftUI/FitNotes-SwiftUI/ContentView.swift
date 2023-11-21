06c728815621df43e66dc6af4f8fb82babd1c998
Artem Marhaza <artemmrgz@gmail.com>
2023-11-21 18:45:53 +0100

    init project

---
+++ FitNotes-SwiftUI/FitNotes-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitNotes-SwiftUI
+//
+//  Created by Artem Marhaza on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitNotes-SwiftUI/FitNotes-SwiftUI/FitNotes_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNotes_SwiftUIApp.swift
+//  FitNotes-SwiftUI
+//
+//  Created by Artem Marhaza on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FitNotes_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
